ab324a6bfb5d29b69253437e5b7481ee0be431b3
Pole <juanmspoleti@gmail.com>
2018-08-03 10:32:41 -0300

    elimino imports sin uso

--- src/test/java/CrowdTestNgRunner.java
+++ src/test/java/CrowdTestNgRunner.java
@@ -1,8 +1,4 @@
 import com.crowdar.bdd.cukes.TestNgRunner;
-import org.testng.annotations.AfterSuite;
-import com.cucumber.listener.Reporter;
-
-import java.io.File;
 
 public class CrowdTestNgRunner extends TestNgRunner {
 
