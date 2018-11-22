fd18a3b1c83d0286176495d40b4c359e7d36bf8c
juarmas <juarmas@gmail.com>
2018-11-22 13:28:14 -0300

    remove startMaximized

--- src/main/java/com/crowdar/web/BrowserConfiguration.java
+++ src/main/java/com/crowdar/web/BrowserConfiguration.java
@@ -49,7 +49,6 @@ public enum BrowserConfiguration {
 
             ChromeOptions options = new ChromeOptions();
             options.addArguments("disable-infobars");
-//            options.addArguments("start-maximized");
             options.addArguments("--ignore-certificate-errors");
 //			options.addArguments("screenshot");
             
