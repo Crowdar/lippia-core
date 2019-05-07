3f75273bd91a7bae443c64207be72fdcdc83d9a2
dario.vallejos <dario.vallejos@crowdaronline.com>
2019-05-07 08:50:35 -0300

    changing mobile configuration

--- src/main/java/com/crowdar/driver/config/MobilePlatformConfiguration.java
+++ src/main/java/com/crowdar/driver/config/MobilePlatformConfiguration.java
@@ -2,16 +2,7 @@ package com.crowdar.driver.config;
 
 import com.crowdar.core.PropertyManager;
 
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.ios.IOSDriver;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import ru.stqa.selenium.factory.SingleWebDriverPool;
-
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public enum MobilePlatformConfiguration implements AutomationConfiguration{
 
@@ -42,15 +33,13 @@ public enum MobilePlatformConfiguration implements AutomationConfiguration{
             capabilities.setCapability("platformName", PropertyManager.getProperty("app.platform"));
             capabilities.setCapability("appPackage", PropertyManager.getProperty("app.package"));
             capabilities.setCapability("autoGrantPermissions", true);
-            capabilities.setCapability("app", System.getProperty("user.dir").concat(File.separator).concat(MOBILE_APP_PATH.concat(File.separator).concat(PropertyManager.getProperty("mobile.app.name"))));
+            capabilities.setCapability("app", PropertyManager.getProperty("crowdar.mobile.apk.path"));
             return capabilities;
         }
 
     };
 
-    private static final String MOBILE_APP_PATH = PropertyManager.getProperty("crowdar.mobile.apk.path");
-
-	public static MobilePlatformConfiguration getPlatformConfiguration(String key) {
+   	public static MobilePlatformConfiguration getPlatformConfiguration(String key) {
 
         for (MobilePlatformConfiguration current : values()) {
             if (current.name().equalsIgnoreCase(key)) {
