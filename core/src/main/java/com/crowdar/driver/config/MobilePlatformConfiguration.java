c7ee03eb183ba8b1839ea4d5b45a5a7070c46101
Pole <juan.spoleti@crowdaronline.com>
2019-12-20 23:43:51 -0300

    1.8.4 + fix capabilities -> saco hardcodeos

--- core/src/main/java/com/crowdar/driver/config/MobilePlatformConfiguration.java
+++ core/src/main/java/com/crowdar/driver/config/MobilePlatformConfiguration.java
@@ -2,32 +2,32 @@ package com.crowdar.driver.config;
 
 import com.crowdar.core.PropertyManager;
 
+import com.crowdar.driver.factory.Capability;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.List;
 
 public enum MobilePlatformConfiguration implements AutomationConfiguration {
 
     ANDROID_APK {
         @Override
         public DesiredCapabilities getDesiredCapabilities() {
-
             DesiredCapabilities capabilities = new DesiredCapabilities();
+
             capabilities.setCapability("deviceName", PropertyManager.getProperty("crowdar.mobile.deviceName"));
             if (!PropertyManager.getProperty("crowdar.mobile.avd").isEmpty()) {
                 capabilities.setCapability("avd", PropertyManager.getProperty("crowdar.mobile.avd"));
             }
-            capabilities.setCapability("browserName", "android");
             capabilities.setCapability("app", PropertyManager.getProperty("crowdar.mobile.apk.path"));
-            capabilities.setCapability("autoGrantPermissions", true);
-            capabilities.setCapability("platformName", "Android");
+            capabilities.setCapability("platformName", PropertyManager.getProperty("crowdar.mobile.platformName"));
             capabilities.setCapability("platformVersion", PropertyManager.getProperty("crowdar.mobile.platformVersion"));
 
-//            if (!PropertyManager.getProperty("crowdar.mobile.appium.newCommandTimeout").isEmpty()) {
-//            }
             capabilities.setCapability("newCommandTimeout", Integer.parseInt(PropertyManager.getProperty("crowdar.mobile.appium.newCommandTimeout")));
             capabilities.setCapability("automationName",PropertyManager.getProperty("crowdar.mobile.automationName"));
-            
-            capabilities.setCapability("unicodeKeyboard", true);
-            capabilities.setCapability("resetKeyboard", true);
+
+            capabilities.setCapability("autoGrantPermissions", "crowdar.mobile.autoGrantPermissions");
+            capabilities.setCapability("unicodeKeyboard", "crowdar.mobile.unicodeKeyboard");
+            capabilities.setCapability("resetKeyboard", "crowdar.mobile.resetKeyboard");
             return capabilities;
         }
     }, ANDROID_CHROME {
