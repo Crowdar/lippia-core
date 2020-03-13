25088668c76d9aca2945be6a58e663c15f36114e
juan.carames <juanmaCarames@asd.com>
2020-03-13 09:48:52 -0300

    remove obsolets

--- core/src/main/java/com/crowdar/driver/config/AutomationConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.crowdar.driver.config;
-
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-public interface AutomationConfiguration {
-
-    public abstract DesiredCapabilities getDesiredCapabilities();
-
-}
