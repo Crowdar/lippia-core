e551d892c7ebcf6b78bc27c9c7b0072e3b315cb3
juan.carames <juanmaCarames@asd.com>
2020-05-22 07:47:26 -0300

    adding hoooks tags feature

---
+++ core/src/main/java/com/crowdar/bdd/cukes/hooks/ZaleniumHook.java
@@ -0,0 +1,32 @@
+package com.crowdar.bdd.cukes.hooks;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.crowdar.driver.DriverManager;
+
+import io.cucumber.core.api.Scenario;
+import io.cucumber.java.Before;
+
+public class ZaleniumHook{
+
+	@Before("@zaleniumHook")
+	public void beforeScenario(Scenario scenario) throws IOException{
+		
+		Map<String, String> extraCapabilities = new HashMap<String, String>();
+		extraCapabilities.put("tz", "America/Argentina/Buenos_Aires");
+		extraCapabilities.put("testFileNameTemplate", "{testName}");
+		extraCapabilities.put("build",System.getProperty("build.identifier"));
+		extraCapabilities.put("name", scenario.getName());
+		
+		try {
+			DriverManager.initialize(extraCapabilities);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+
+}
