bf34ff70e8888208c10823b74863d57299b5cebc
lgtmboop <alejo.carbonari@crowdaronline.com>
2023-06-02 19:00:17 +0000

    update under test

--- core/src/main/java/com/crowdar/core/EnvironmentManager.java
+++ core/src/main/java/com/crowdar/core/EnvironmentManager.java
@@ -1,56 +1,62 @@
 package com.crowdar.core;
 
-
 import com.crowdar.core.PropertyManager;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import com.typesafe.config.ConfigRenderOptions;
 import com.typesafe.config.ConfigValue;
-import io.lippia.api.lowcode.recognition.parser.Deserialization;
+
+import io.lippia.api.lowcode.exception.LippiaException;
 
 public class EnvironmentManager {
-
-    private static String definedEnvironment;
-    private static JsonObject resources;
+    private static final String definedEnvironment;
+    private static final JsonObject resources;
     private static JsonObject resource;
-
-    static {
-        definedEnvironment = PropertyManager.getProperty("environment");
-        resources          = deserialize(loadConfiguration());
-        resource           = mapResources();
-    }
 
     private EnvironmentManager() {}
 
-    private static Config loadConfiguration() {
+    static {
+        definedEnvironment = PropertyManager.getProperty("environment");
+        resources = parseFileAsJsonObject(loadFileConfiguration());
+    }
+
+    private static Config loadFileConfiguration() {
         return ConfigFactory.parseResources("lippia.conf");
     }
 
-    private static JsonObject deserialize(Config configuration) {
+    private static JsonObject parseFileAsJsonObject(Config configuration) {
         ConfigValue configValue = configuration.getValue("environments");
         String jsonString = configValue.render(ConfigRenderOptions.concise());
-        return Deserialization.getJsonObject(jsonString);
+        return new Gson().fromJson(jsonString, JsonObject.class);
     }
 
     public static String getProperty(String property) {
+        parseResources();
         if (resource.has(property)) {
             return resource.getAsJsonPrimitive(property).getAsString();
         }
 
-        throw new RuntimeException("Property {" + property + "} has not been defined in " + definedEnvironment + " in lippia.conf");
+        throw new LippiaException("Property {" + property + "} has not been defined in " + definedEnvironment + " in lippia.conf");
     }
 
-    private static JsonObject mapResources() {
-        if (resources.has(definedEnvironment)) {
-            resource = resources.getAsJsonObject(definedEnvironment);
-        } else if (resources.has("default")) {
-            resource = resources.getAsJsonObject("default");
+    private static void parseResources() {
+        String[] environments = definedEnvironment.split("#");
+        for (String env: environments) {
+            parseResources(env);
+        }
+    }
+
+    private static void parseResources(String env) {
+        if (resources.has(env) && resource == null) {
+            resource = resources.getAsJsonObject(env);
+        } else if (resource.has(env)) {
+            resource = resource.getAsJsonObject(env);
         }
 
-        if (!resource.isJsonNull()) return resource;
+        if (resource != null) return;
 
-        throw new RuntimeException(definedEnvironment + " & default environments have not been defined in lippia.conf");
+        throw new LippiaException(env + " environment have not been defined in lippia.conf");
     }
-
 }
